3d8325ee2b1aeb84d7eb31adbd669cc0f0916ed4
4288 <a4288@MacBook-Pro-de-4288.local>
2025-02-24 23:22:24 -0600

    changes

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiURL: 'https://fakestoreapi.com',
+  stripeAPIKey: 'pk_test_51QvsiWJovX47gsxLNGMiWwuksKYZAVwv5YdJYS2vzwYnyEpxdZbeuS7QCdroUEr529yM2dOPHIfpYzgWmoz30D0S0068L8eYnt',
+  serverURL: 'http://localhost:4242',
+};
